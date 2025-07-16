a3fdd3d081348629eb23701929a43a16737d5a79
ZhangIII <1091202708@163.com>
2025-07-16 16:44:29 +0800

    Docx inlined doc

--- file_analyzer.py
+++ file_analyzer.py
@@ -16,6 +16,12 @@ def Doc_to_Docx(path):
     doc.Close()
     word.Quit()
 
+def Check_Inline_File(path):
+    #个别文件中存在内嵌文件对象
+    #基于docx本质是zip的原理，解析其embeddings目录下是否存在文件
+
+    pass
+
 def Figure_doc(File_Path):
     EXT=File_Path.split('.')[-1]
     if EXT == 'doc':  #先将doc转化成docx
@@ -25,20 +31,39 @@ def Figure_doc(File_Path):
     doc=docx.Document(File_Path)  #读取word文件
     paragraphs = doc.paragraphs   #处理文字内容
     for para in paragraphs:
-        if para.text.find("无人机") != -1:
-            print(para.text)
+        if any(kw in para.text for kw in Keyword_list):
+            pass
+            #print(para.text)
+            # return 1
 
     tables=doc.tables  #处理表格内容
     for table in tables:
         for row in table.rows:
             for cell in row.cells:
                 if any(kw in cell.text for kw in Keyword_list):
-                    print(cell.text)
+                    pass
+                    #print(cell.text)
+                    # return 1
+
+
+    return Check_Inline_File(File_Path)
 
 
 
+def Figure_xls(File_Path):
+    pass
+
+
+
+def Figure_pdf(File_Path):
+    pass
+
+
+
+
+# 返回值 1：符合筛选条件  2：不符合筛选条件  3：内部存在未知文件，需要人工核查
 if __name__ == "__main__":
-    Figure_doc('Test_doc/table.docx')
+    Figure_doc('Test_doc/object_test.docx')
 
 
 
